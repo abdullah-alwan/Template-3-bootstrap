a63122e23a9d5b35efdec4c3e284cb6c16b80e48
abdullah-alwan <130040067+abdullah-alwan@users.noreply.github.com>
2023-05-14 12:41:18 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// document.getElementById("btn1").onclick=function(){
+//     document.getElementById("div1").innerHTML="dsdf"
+    
+// }
+// console.log(100*5)
+// alert ("125")
+// prompt ("enter")
+// var x=70
+//     x=90
+//     console.log(-x)
+//     console.log(typeof x)
+
+// var x= [ "dfd", 8 , ali  ,"fhhh" ]
+
+// console.log(typeof x[2])
+// var y=50
+
+// console.log(y*false)
+
